1164a0e68df223c4241adb12c61b5a10eec73a07
jacobhsu <jacob.hsu.tw@gmail.com>
2022-09-02 14:38:12 +0800

    feat: BiFilterAlt

--- src/pages/nft.js
+++ src/pages/nft.js
@@ -3,13 +3,21 @@ import { Box } from '@mui/material';
 
 import { NFTCard } from 'components/NFTCard';
 import { SearchSider } from 'components/nft/SearchSider';
-
+import { Dropdown } from 'react-bootstrap';
 import { goods } from '../data/goods';
 import { CardList } from 'theme/nft';
-import 'style/SearchSider.css'
+import {
+  BiSortDown,
+  BiFilterAlt,
+  BiDownArrowAlt,
+  BiUpArrowAlt,
+  BiSortUp,
+} from 'react-icons/bi';
+import 'style/SearchSider.css';
 
 export default function Home() {
   const [products, setProducts] = useState(goods);
+  const [sort, setSort] = useState('oldest');
   return (
     <Box
       sx={{
@@ -18,7 +26,42 @@ export default function Home() {
         alignItems: 'center',
       }}
     >
-      <SearchSider/>
+      <SearchSider />
+      <Dropdown id="dropdown-sort" style={{ width: '68%', textAlign: 'right' }}>
+        <Dropdown.Toggle variant="light" id="dropdown-basic">
+          <BiFilterAlt /> Filiter 
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item
+            onClick={() => {
+              setSort('oldest');
+            }}
+          >
+            Oldest <BiDownArrowAlt />
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              setSort('newest');
+            }}
+          >
+            Newest <BiUpArrowAlt />
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              setSort('lowerPrice');
+            }}
+          >
+            Price <BiSortDown />
+          </Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              setSort('biggerPrice');
+            }}
+          >
+            Price <BiSortUp />{' '}
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
       <CardList container spacing={0}>
         {products
           ? products.map((item, index) => <NFTCard {...{ item }} key={index} />)
